c398b150595c9cc66cb12454accbe3bdda509ad6
Syfur Rahman <43377197+syfurrahman@users.noreply.github.com>
2021-04-12 17:51:10 -0400

    Added the props and function Added the props and function

--- src/App.js
+++ src/App.js
@@ -5,6 +5,27 @@ export default function App() {
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
+
+      <Cards color="yellow" num="5" />
     </div>
   );
 }
+
+function Cards(props) {
+  return (
+    <div>
+      This card is a {props.color} {props.num}
+    </div>
+  );
+}
+
+function deck(props) {}
+
+// function cardColor(props)
+// {
+//     Red,
+//     Blue,
+//     Yellow,
+//     Green,
+//     Wild
+// }
