ba8802ebd100b7a6c4b8fc2f66eb38e55ddda911
Julianj22 <42346111+Julianj22@users.noreply.github.com>
2021-05-10 18:00:15 -0400

    players

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
+import React, { useState } from "react";
 import "./styles.css";
 
+const numPlayers = 4;
+const gameDeck = deck();
+const gameDeal = dealDeck(gameDeck);
+
 export default function App() {
-  let gameDeck = deck();
-  let gameDeal = dealDeck(gameDeck)
+  let [turn, setTurn] = useState(0);
+  let [player1, setPlayer1] = useState(gameDeal[0]);
+  let [player2, setPlayer2] = useState(gameDeal[1]);
+  let [player3, setPlayer3] = useState(gameDeal[2]);
+  let [player4, setPlayer4] = useState(gameDeal[3]);
+  let [drawPile, setDrawPile] = useState(gameDeal[4]);
+  let playerStates = [player1, player2, player3, player4];
+  let setPlayerStates = [setPlayer1, setPlayer2, setPlayer3, setPlayer4];
   return (
     <div className="App">
-      <h1>Player 1</h1>
-      {gameDeal[0]}
-      <h1>Player 2</h1>
-      {gameDeal[1]}
-      <h1>Player 3</h1>
-      {gameDeal[2]}
-      <h1>Player 4</h1>
-      {gameDeal[3]}
+      <h1>Current turn: Player {turn + 1}</h1>
+      <h1>Player {turn + 1}'s Hand:</h1>
+      {playerStates[turn]}
+      <Draw
+        pile={drawPile}
+        player={gameDeal[turn]}
+        setTurn={setTurn}
+        turn={turn}
+      />
     </div>
   );
 }
@@ -30,27 +42,44 @@ function Cards(props) {
 //         first four are hands for players
 //         remainder is the draw pile
 
-function dealDeck(deck){
-
-  var hand = [[],[],[],[]];
+function dealDeck(deck) {
+  var hand = [[], [], [], []];
 
   for (let p = 0; p < 4; p++) {
-   for (let i = 0; i < 7; i++) {
-		
-    var randHand = Math.floor(Math.random() * deck.length);
+    for (let i = 0; i < 7; i++) {
+      var randHand = Math.floor(Math.random() * deck.length);
 
+      hand[p].push(...deck.slice(randHand, randHand + 1));
 
-      hand[p].push(...deck.slice(randHand, randHand+1));
-    
       deck.splice(randHand, 1);
-    
     }
   }
 
   return [...hand, deck];
-};
-  
+}
 
+function dealDrawPile(pile, player) {
+  var pileDraw = Math.floor(Math.random() * pile.length);
+
+  player.push(...pile.slice(pileDraw, pileDraw + 1));
+
+  pile.splice(pileDraw, 1);
+}
+
+function Draw(props) {
+  return (
+    <div>
+      <button
+        onClick={() => {
+          dealDrawPile(props.pile, props.player);
+          props.setTurn((props.turn + 1) % numPlayers);
+        }}
+      >
+        Draw
+      </button>
+    </div>
+  );
+}
 
 let colors = ["red", "blue"];
 let nums = ["1", "2"];
@@ -89,5 +118,3 @@ function deck() {
 let cards = deck();
 
 console.log(cards);
-
-
